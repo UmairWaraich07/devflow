43d23fc94e9d4bacd2081987d8c9903628bfe630
Umair <umairwaraich2019@gmail.com>
2023-11-23 14:53:57 +0500

    added the generate ai answer

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -17,6 +17,7 @@ import { useTheme } from "@/context/ThemeProvider";
 import Image from "next/image";
 import { createAnswer } from "@/lib/actions/answer.action";
 import { usePathname } from "next/navigation";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
 interface Props {
   questionId: string;
@@ -29,6 +30,7 @@ const Answer = ({ questionId, authorId, question }: Props) => {
   const { mode } = useTheme();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isAISubmitting, setIsAISubmitting] = useState(false);
   const form = useForm<z.infer<typeof answerSchema>>({
     resolver: zodResolver(answerSchema),
     defaultValues: {
@@ -60,6 +62,33 @@ const Answer = ({ questionId, authorId, question }: Props) => {
     }
   }
 
+  const generateAIAnswer = async () => {
+    // if (!authorId) return;
+    setIsAISubmitting(true);
+    try {
+      console.log("Inside AI answer");
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+        {
+          method: "POST",
+          body: JSON.stringify({ question }),
+        }
+      );
+      const aiAnswer = await response.json();
+      // convert plain text to html format
+      const formattedText = aiAnswer.reply.replace(/\n/g, "<br />");
+      if (editorRef.current) {
+        const editor = editorRef.current as any;
+        editor.setContent(formattedText);
+      }
+    } catch (error) {
+      console.log(`Error while generating AI answer ${error}`);
+      throw error;
+    } finally {
+      setIsAISubmitting(false);
+    }
+  };
+
   return (
     <div>
       <div className="mt-7 flex w-full flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -69,16 +98,23 @@ const Answer = ({ questionId, authorId, question }: Props) => {
 
         <Button
           className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none "
-          onClick={() => {}}
+          onClick={generateAIAnswer}
+          disabled={isAISubmitting}
         >
-          <Image
-            src="/assets/icons/stars.svg"
-            alt="stars"
-            width={12}
-            height={12}
-            className="object-contain"
-          />
-          Generate an AI answer
+          {isAISubmitting ? (
+            <ReloadIcon className="w-4 h-4 object-contain text-primary-500 animate-spin" />
+          ) : (
+            <Image
+              src="/assets/icons/stars.svg"
+              alt="stars"
+              width={12}
+              height={12}
+              className="object-contain"
+            />
+          )}
+          <p className="cursor-pointer">
+            {isAISubmitting ? "Generating..." : "Generate AI answer"}
+          </p>
         </Button>
       </div>
       <Form {...form}>
